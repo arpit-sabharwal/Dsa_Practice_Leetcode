959095cd7b8f692a35eb986cc2ab6e889892872c
arpit-sabharwal <95696385+arpit-sabharwal@users.noreply.github.com>
2022-01-05 15:41:40 +0530

    Time: 22 ms (5.16%), Space: 10.4 MB (71.25%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -11,29 +11,44 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
+//         if(!head||!head->next||!head->next->next)
+//             return head;
+//         ListNode* oh=head;
+//         ListNode* eh=head->next;
+//         ListNode* curr=head->next->next;
+//         int count=0;
+//         ListNode* even=eh;
+//         ListNode* odd=oh;
+//         while(curr){
+//             count++;
+//             if(count%2){
+//                 odd->next=curr;
+//                 odd=odd->next;
+//             }
+//             else{
+//                 even->next=curr;
+//                 even=even->next;
+//             }
+//             curr=curr->next;
+//         }
+//         odd->next=eh;
+//         even->next=NULL;
+//         return head;
+         //solution 1 end
         if(!head||!head->next||!head->next->next)
-            return head;
-        ListNode* oh=head;
+             return head;
+        ListNode* odd=head;
+        ListNode* even=head->next;
         ListNode* eh=head->next;
-        ListNode* curr=head->next->next;
-        int count=0;
-        ListNode* even=eh;
-        ListNode* odd=oh;
-        while(curr){
-            count++;
-            if(count%2){
-                odd->next=curr;
-                odd=odd->next;
-            }
-            else{
-                even->next=curr;
-                even=even->next;
-            }
-            curr=curr->next;
+        
+        while(odd->next&&even->next){
+            odd->next=even->next;
+            even->next=odd->next->next;
+            odd=odd->next;
+            even=even->next;
         }
         odd->next=eh;
-        even->next=NULL;
         return head;
-        
+             
     }
 };
